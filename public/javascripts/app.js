775030befbdac733456f2ab7c9315f84f8b0eb13
Michael Aranda <michaelaranda0.web@gmail.com>
2017-05-04 23:16:23 -0700

    Restructure app to work better with babel and keep dev/prod app.js paths consistent.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -16,7 +16,7 @@ let users = require('./routes/users');
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../../views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../../public')));
 
 // Make our db accessible to our router
 app.use((req,res,next) => {
