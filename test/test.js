22491da7c5aae0de8cc09adc8882c292eb594099
Michael Aranda <michaelaranda0.web@gmail.com>
2018-04-27 23:27:28 +0000

    Adds config option to Mocha script to allow testing of ES6 code.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,37 @@
 var assert = require('assert');
 
-describe('only one restaurant has a meh preference', function() {
-  def('testArray', function() { return [1,2,3] });
+import { sortRestaurantsByLunchGroupPreferences } from "../helpers/restaurantHelper";
+
+function restaurantFixture(name, mehs, nos) {
+  return [
+    { 
+      "name": name || "Test Restaurant", 
+      "mehs": mehs || [], 
+      "nos": nos || []
+    }
+  ]
+}
+
+function userFixture(name) {
+  var username = name || "Test User";
   
-  it('should return -1 when the value is not present', function() {
-    assert.equal($testArray.indexOf(4), -1);
+  return {"name": username}
+}
+
+var user = userFixture();
+
+describe('#sortRestaurantsByLunchGroupPreferences', function() {
+  def('restaurants', function() { 
+    return [
+      restaurantFixture("A"),
+      restaurantFixture("B"),
+      restaurantFixture("C")
+    ] 
+  });
+  
+  describe('only one restaurant has a meh preference', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal($restaurants.length, 3);
+    });
   });
 });
