e0701cfc06adbb3fef133808317d83f291bbad8a
Michael Aranda <michaelaranda0.web@gmail.com>
2018-05-04 22:28:40 +0000

    Adds icons to switch between comment list and add comment section on General Tab Content.

--- app/assets/javascripts/components/home_page/restaurant_item/RestaurantItemGeneralTabContent.jsx
+++ app/assets/javascripts/components/home_page/restaurant_item/RestaurantItemGeneralTabContent.jsx
@@ -2,30 +2,83 @@ import React from 'react';
 import UserComment from './UserComment.jsx';
 
 export class RestaurantItemGeneralTabContent extends React.Component {
-  renderComments() {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      showComments: true
+    };
+  }
+  
+  renderContent() {
     if (this.props.restaurant.comments) {
       return (
         <div className="comments-section">
-          <h4 className="comments-header">Comments</h4>
-          {
-            this.props.restaurant.comments.map((comment, i) => {
-              return (
-                <div key={i}>
-                  <UserComment comment={comment} />
-                  <hr />
-                </div>
-              )
-            })
-          }
+          <div className="header-section">
+            <h4 className="comments-header">Comments</h4>
+            {this.renderHeaderIcon()}
+          </div>
+          {this.renderSection()}
         </div>
       );
+    } 
+  }
+  
+  renderSection() {
+    if(this.state.showComments) {
+      return this.renderCommentsList();
+    } else {
+      return this.renderAddCommentSection();
     }
+  }
+  
+  renderCommentsList() {
+    return (
+      this.props.restaurant.comments.map((comment, i) => {
+        return (
+          <div key={i}>
+            <UserComment comment={comment} />
+            <hr />
+          </div>
+        )
+      })
+    );
+  }
+  
+  renderAddCommentSection() {
+    return (
+      <div className="add-comment-section">
+        Add Comment Section
+      </div>
+    );
+  }
+  
+  renderHeaderIcon() {
+    if(this.state.showComments) {
+      return (
+        <div className="add-comment-button glyphicon glyphicon-plus"
+              onClick={this.showAddCommentSection.bind(this)}></div>
+      )
+    } else {
+      return (
+        <div className="return-button glyphicon glyphicon-arrow-left"
+              onClick={this.showCommentsList.bind(this)}></div>
+      )
+    }
+  }
+  
+  showCommentsList() {
+    this.setState({ showComments: true });
+  }
+  
+  showAddCommentSection() {
+    this.setState({ showComments: false });
   }
   
   render() {
     return (
       <div className="general-tab-content">
-        {this.renderComments()}
+        {this.renderContent()}
       </div>
     );  
   }
